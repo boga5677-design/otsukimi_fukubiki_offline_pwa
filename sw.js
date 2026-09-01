a1273004cf6edbc92acde3fda83cc3d6faad0dc8
boga5677-design <boga5677@gmail.com>
2026-09-01 17:26:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "otsukimi-fukubiki-v7";
+const CACHE = "otsukimi-fukubiki-v7-4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
